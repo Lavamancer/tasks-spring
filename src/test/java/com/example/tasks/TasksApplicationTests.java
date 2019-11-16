eee7a7494b1a430457e4fefcc7bbd7db5558f234
Juan Albarracin <jalbarracinq@gmail.com>
2019-11-16 16:13:32 +0100

    init

---
+++ src/test/java/com/example/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
